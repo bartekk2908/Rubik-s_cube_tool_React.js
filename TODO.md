5f030fd02efc23889f0a35b4f2cfb97e5fce2f77
Bartosz Krzysztoszek <bartekk2908@op.pl>
2023-10-21 17:19:45 +0200

    scramble generator, initial timer + TODO.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+
+- działanie aplikacji w tle
+- włączanie timera klawiszami klawiatury
+- baza danych z czasami
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <>
+            <ScrambleField n={20}/>
+            <TimerField/>
+        </>
+    );
 }
 
-export default App;
+function ScrambleField({ n }) {
+    const [scrambleText, setScrambleText] = useState(generateScramble(n));
+
+    return (
+        <>
+            <div>
+                <button onClick={() => setScrambleText(generateScramble(n))}>next scramble</button>
+            </div>
+            <div>{scrambleText}</div>
+        </>
+    );
+}
+
+function generateScramble(n) {
+    const faces = ['F', 'B', 'R', 'L', 'U', 'D'];
+    const modifiers = ["", "'", "2"];
+    let scramble = "";
+    let face = "";
+    let lastFace = "";
+    let nextToLastFace = "";
+    for (let i = 0; i<n; i++) {
+    // choose again if: chosen face is the same as last face OR chosen face is the same as next to last face and last face is opposite face to chosen face
+    do {
+        face = faces[Math.floor(Math.random()*faces.length)];
+    } while (lastFace === face || (nextToLastFace === face && lastFace === (faces.indexOf(face)%2 ? faces[faces.indexOf(face)-1] : faces[faces.indexOf(face)+1])))
+    nextToLastFace = lastFace;
+    lastFace = face;
+    scramble += face + modifiers[Math.floor(Math.random()*modifiers.length)] + " ";
+    }
+    return scramble;
+}
+
+function TimerField() {
+    // time in 10 ms unit
+    const [time, setTime] = useState( 0);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const hours = Math.floor(time / 360_000);
+    const minutes = Math.floor((time % 360_000) / 6_000);
+    const seconds = Math.floor((time % 6_000) / 100);
+    const milliseconds = Math.floor(time % 100);
+
+    useEffect(() => {
+        let intervalId;
+        if (isRunning) {
+            intervalId = setInterval(() => setTime(time + 1), 10);
+        }
+        return () => clearInterval(intervalId);
+    }, [isRunning, time])
+
+    function startAndStop() {
+        if (!isRunning) {
+            resetTime();
+        }
+        setIsRunning(!isRunning);
+    }
+
+    function resetTime() {
+        setTime(0);
+    }
+
+    return (
+        <>
+            <div>
+                {hours ? `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}` : (minutes ? `${minutes}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}` : `${seconds}.${milliseconds.toString().padStart(2, '0')}` )}
+            </div>
+            <button onClick={startAndStop}>{isRunning ? "Stop" : "Start"}</button>
+        </>
+    );
+}
+
