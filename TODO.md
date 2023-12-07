afe172992f851eae6b90db9f059f2a41392181ef
Bartosz Krzysztoszek <bartekk2908@op.pl>
2023-12-07 01:24:31 +0100

    todo, small fixes

--- TODO.md
+++ TODO.md
@@ -1,3 +1,10 @@
 
-- statsy dla algorytmów
-- sortowanie tabelki
+- testy wszystkich algorytmów
+- testy na nowej przeglądarce
+- best i worst algorithms naprawić/zmienić
+- przewijanie się results zbadać
+- wyświetlanie się nazwy algorytmu na liście obok czasu
+- aktualizacja help
+- algorytmy z ruchami warstwą M
+- testy instalacji na innym sprzęcie itd
+- kiedyś: sortowanie tabelki
--- src/Stats.js
+++ src/Stats.js
@@ -26,7 +26,6 @@ export function Stats({ results, timerTab, algorithmsData }) {
             const algorithmTimes = results[timerTab].filter((result) => result.algorithmName === chosenAlgorithmsData[i][0]).map((result) => result.time);
             averages.set(chosenAlgorithmsData[i][0], averageOfLastX(algorithmTimes?.length, algorithmTimes, true) / chosenAlgorithmsData[i][1].split(" ").length)
         }
-        console.log(averages);
         let minAlgorithm = undefined;
         let maxAlgorithm = undefined;
         for (const [key, value] of averages.entries()) {
